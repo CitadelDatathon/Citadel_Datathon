e87233a549cbf8963bef25f4dd2d327b78bdbd24
ZHE WANG <z687wang@edu.uwaterloo.ca>
2018-09-29 15:19:54 -0400

    final report :fire:

--- src/education_attainment/load_data.py
+++ src/education_attainment/load_data.py
@@ -5,26 +5,18 @@ import json
 education_attainment_file = 'education_attainment.csv'
 
 
-data_headers = ['fips','state','county', 'year', 'less_than_hs', 'hs_diploma', 'some_college_or_associates', 'college_bachelors_or_higher', 'pct_less_than_hs', 'pct_hs_diploma	pct_college_or_associates', 'pct_college_bachelors_or_higher']
 
-result_headers = ['less_than_hs','hs_diploma','some_college_or_associates','college_bachelors_or_higher','pct_less_than_hs','pct_hs_diploma','pct_college_or_associates','pct_college_bachelors_or_higher']
-df_withNA =  pd.read_csv(education_attainment_file, encoding='latin-1')
+result_headers = ['year', 'less_than_hs', 'hs_diploma', 'some_college_or_associates', 'college_bachelors_or_higher', 'pct_less_than_hs', 'pct_hs_diploma', 'pct_college_or_associates', 'pct_college_bachelors_or_higher']
+df =  pd.read_csv(education_attainment_file, encoding='latin-1')
 
-df = df_withNA.dropna()
-
-
-countys = df['county']
-fips = df['fips']
-years = df['year']
-
-print(fips)
+print(df)
 
 
-def education_attainment_fips_year(fips, year):
-    if(int(year) >= 2012 and int(year) <= 2016):
-        print("2012-2016")
-        year = "2012-2016"
-    selected = df.loc[(df['fips'] == fips) & (df['year'] == year)]
+def education_attainment_fips_year(fips):
+    selected = df.loc[(df['fips'] == fips)]
+    print(selected)
     return selected
 
-# education_attainment_fips_year(1003, 1970)
+
+
+
