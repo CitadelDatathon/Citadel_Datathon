a56d98641f6cf68a71fef638bcb5c1a7b2f54359
Leo Zhu <lichen.zhu@oracle.com>
2018-09-29 12:38:37 -0400

    all

---
+++ earning_json.py
@@ -0,0 +1,45 @@
+import os
+import pandas as pd
+import json
+from datetime import datetime as dt
+
+PATH = os.getcwd()
+DATA_DIR = os.path.join(PATH, 'Datathon Materials 2')
+DROUGHT_DIR = os.path.join(DATA_DIR, 'droughts.csv')
+EARNING_DIR = os.path.join(DATA_DIR, 'earnings.csv')
+EDU_DIR = os.path.join(DATA_DIR, 'education_attainment.csv')
+INDUSTRY_DIR = os.path.join(DATA_DIR, 'industry_occupation.csv')
+CHEM_DIR = os.path.join(DATA_DIR, 'chemicals.csv')
+
+df_earn = pd.read_csv(EARNING_DIR)
+
+data = {}
+for index, row in df_earn.iterrows():
+    fips = row['fips']
+    none = row['none']
+    d0 = row['d0']
+    d1 = row['d1']
+    d2 = row['d2']
+    d3 = row['d3']
+    d4 = row['d4']
+
+    valid_start = dt.strptime(row['valid_start'], '%Y-%m-%d')
+    year = valid_start.year
+
+    if fips not in data:
+        data[fips] = {}
+
+    if year not in data[fips]:
+        data[fips][year] = [none, d0, d1, d2, d3, d4]
+    else:
+        data[fips][year][0] += none
+        data[fips][year][1] += d0
+        data[fips][year][2] += d1
+        data[fips][year][3] += d2
+        data[fips][year][4] += d3
+
+
+with open('drought.json', 'w') as outfile:
+    json.dump(data, outfile)
+
+
--- predict_model.py
+++ predict_model.py
@@ -11,43 +11,85 @@ with open('chem_2.json') as f:
 with open('drought.json') as f:
     drought_data = json.load(f)
 
+with open('industry_occupation.json') as f:
+    indus_data = json.load(f)
+
+with open('earnings.json') as f:
+    earning_data = json.load(f)
+
+with open('earnings.json') as f:
+    earning_data = json.load(f)
+
+
 
 train_data = []
 train_labels = []
 
-testing_data = []
-testing_labels = []
+test_data = []
+test_labels = []
 
+counter = 0
 for fip in drought_data:
     for year in drought_data:
         if fip not in chem_data or year not in chem_data[fip]:
             continue
-        data= chem_data[fip][year]
+        if fip not in indus_data or year not in indus_data[fip]:
+            continue
+        if fip not in earning_data or year not in earning_data[fip]:
+            continue
+
+        data = []
+        data.append(int(fip))
+        counter += 1
+        data.extend(chem_data[fip][year])
+
+        for data123 in indus_data[fip][year]:
+            data.append(int(data123))
+
+        for data123 in earning_data[fip][year]:
+            if isinstance(data123, str):
+                data123  = data123.replace('+', '').replace(',', '').replace('-', '')
+            data.append(int(data123))
+
         label = drought_data[fip][year]
 
         if int(year) < 2015:
             train_data.append(data)
             train_labels.append(label)
         else:
-            testing_data.append(data)
-            testing_labels.append(label)
+            test_data.append(data)
+            test_labels.append(label)
 
 train_data = array(train_data)
 train_labels = array(train_labels)
 
-print(train_data)
-print(train_labels)
+test_data = array(test_data)
+test_labels = array(test_labels)
+
+print(test_data)
 
 mean = train_data.mean(axis=0)
 std = train_data.std(axis=0)
-train_data = (train_data - mean) / std
 
+train_data = (train_data - mean) / std
+test_data = (test_data - mean) / std
+
+print(test_data)
+#print(train_data)
+#print(test_data)
+
+def get_length(map_data):
+    for fip in map_data:
+        for year in map_data[fip]:
+            return len(map_data[fip][year])
+
+dim = 1 + get_length(indus_data) + get_length(earning_data)
 
 def build_model():
     model = keras.Sequential([
-    keras.layers.Dense(6, activation=tf.nn.relu,
+    keras.layers.Dense(dim , activation=tf.nn.relu,
                        input_shape=(train_data.shape[1],)),
-    keras.layers.Dense(6, activation=tf.nn.relu),
+    keras.layers.Dense(dim, activation=tf.nn.relu),
     keras.layers.Dense(6)
     ])
     optimizer = tf.train.RMSPropOptimizer(0.001)
@@ -57,14 +99,20 @@ def build_model():
     return model
 
 model = build_model()
-model.summary()
+EPOCHS = 500
 
-EPOCHS = 50
+early_stop = keras.callbacks.EarlyStopping(monitor='val_loss', patience=20)
+class PrintDot(keras.callbacks.Callback):
+    def on_epoch_end(self, epoch, logs):
+        if epoch % 100 == 0: print('')
+        print('.', end='')
 
-# Store training stats
 history = model.fit(train_data, train_labels, epochs=EPOCHS,
-                    validation_split=0.2, verbose=0)
+                    validation_split=0.2, verbose=0,
+                    callbacks=[early_stop, PrintDot()])
 
+
+model.summary()
 
 def plot_history(history):
     plt.figure()
@@ -75,7 +123,6 @@ def plot_history(history):
     plt.plot(history.epoch, np.array(history.history['val_mean_absolute_error']),
            label = 'Val loss')
     plt.legend()
-    plt.ylim([0, 5])
     plt.show()
 
 plot_history(history)
@@ -83,4 +130,19 @@ plot_history(history)
 
 
 
+test_predictions = model.predict(test_data).flatten()
+plt.scatter(test_labels, test_predictions)
+plt.xlabel('True Values [1000$]')
+plt.ylabel('Predictions [1000$]')
+plt.axis('equal')
+plt.xlim(plt.xlim())
+plt.ylim(plt.ylim())
+plt.plot([-100, 100], [-100, 100])
+plt.show()
 
+
+error = test_predictions - test_labels.flatten()
+plt.hist(error, bins = 50)
+plt.xlabel("Prediction Error [1000$]")
+plt.ylabel("Count")
+plt.show()
